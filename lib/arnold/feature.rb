9f9ca2b9b97608339237a3cb10a10d5c1472bf25
@rubynerd <x@rubynerd.net>
2020-10-24 22:49:10 +0100

    fix relationship name

--- lib/arnold/feature.rb
+++ lib/arnold/feature.rb
@@ -1,5 +1,5 @@
 class Arnold::Feature < Arnold::Model
-  has_many :features
+  has_many :overrides
 
   # attr_accessor :id, :name, :identifier, :bucket_type, :target_groups, :active, :variants, :winning_variant, :overrides, :fixed_determinations
 end
