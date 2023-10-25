19e33bd37d5afafdee6674721d91bf3776b928e1
@rubynerd <x@rubynerd.net>
2023-10-25 20:24:50 +0100

    render react in html

--- ui/index.ts
+++ ui/index.ts
@@ -1,1 +1,6 @@
-console.log('hello from some kind of javascript');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('react-root'));
+
+root.render(<h1>Hello from React!</h1>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default {
   entry: './ui/index.ts',
   output: {
     path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'lib', 'arnold', 'app', 'public'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  mode: isProduction ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 };
