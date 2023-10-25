dadda28c84444961a2c0d3622862adc6dcb83948
@rubynerd <x@rubynerd.net>
2023-10-25 19:03:43 +0100

    fix feature debacle

--- lib/arnold/feature.rb
+++ lib/arnold/feature.rb
@@ -2,7 +2,7 @@ class Arnold::Feature < Arnold::Model
   has_many :overrides
   has_many :target_groups
 
-  enum bucket_type: %i{id guid}
+  enum bucket_type: %i{id guid}, _prefix: :true
 
   # attr_accessor :id, :name, :identifier, :bucket_type, :target_groups, :active, :variants, :winning_variant, :overrides, :fixed_determinations
 
